c007f096198348229eb2a67912135957a1776fff
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-04-29 11:05:09 +0300

    Create Weather Entity

--- app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
@@ -16,10 +16,17 @@
 
 package com.example.android.sunshine.data.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Date;
 
+@Entity(tableName = "weather", indices = {@Index(value = {"date"}, unique = true)})
 public class WeatherEntry {
 
+    @PrimaryKey(autoGenerate = true)
     private int id;
     private int weatherIconId;
     private Date date;
@@ -29,6 +36,30 @@ public class WeatherEntry {
     private double pressure;
     private double wind;
     private double degrees;
+
+    /**
+     * This constructor is used by Room
+     * @param id
+     * @param weatherIconId Image id for weather
+     * @param date Date of weather
+     * @param min Min temperature
+     * @param max Max temperature
+     * @param humidity Humidity for the day
+     * @param pressure Barometric pressure
+     * @param wind Wind speed
+     * @param degrees Wind direction
+     */
+    public WeatherEntry(int id, int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
+        this.id = id;
+        this.weatherIconId = weatherIconId;
+        this.date = date;
+        this.min = min;
+        this.max = max;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.wind = wind;
+        this.degrees = degrees;
+    }
 
     /**
      * This constructor is used by OpenWeatherJsonParser. When the network fetch has JSON data, it
@@ -42,6 +73,7 @@ public class WeatherEntry {
      * @param wind Wind speed
      * @param degrees Wind direction
      */
+    @Ignore
     public WeatherEntry(int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
         this.weatherIconId = weatherIconId;
         this.date = date;
